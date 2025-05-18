e5060153abb5b0448b95e39528491e42a2f7b66b
Tevin Mukudi <deskcomp10@outlook.com>
2025-05-18 17:15:55 -0400

    program start bash script

---
+++ program_start.sh
@@ -0,0 +1,6 @@
+
+# source the virtual environment
+source /home/raspberrypi/sensor_program/bin/activate
+
+# start the program
+python3 /home/raspberrypi/sensor_program/device_prototype/sensor_test.py
